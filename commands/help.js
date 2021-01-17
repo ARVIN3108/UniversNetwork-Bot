60f9acb6cc8ebb9f11ae7054e01be4a99f152076
ARVIN3108 <70049331+ARVIN3108@users.noreply.github.com>
2021-01-17 16:59:30 +0700

    Add files via upload

--- commands/help.js
+++ commands/help.js
@@ -4,14 +4,14 @@ module.exports = {
     execute(message, MessageEmbed) {
         if (message.author.id === '700166055326384179') {
             const embed = new MessageEmbed()
-            .setColor('#02C2FF')
-            .setAuthor('UniversNetwork', 'https://cdn.discordapp.com/app-icons/792994169659981846/63db7674caa58a0694d989693bc4b60a.png?size=64', 'https://minecraft-mp.com/server-s272254')
-            .setTitle('**Prefix:** `.`')
-            .addField('\u200B', '\u200B', true)
-            .addField('Developer List Of Commands', '\u200B', true)
-            .addField('\u200B', '\u200B', true)
-            .addField('`.reload` `.load`', 'Untuk me-reload semua command')
-            .setFooter('Made By ARVIN3108 ID', 'https://cdn.discordapp.com/avatars/700166055326384179/3ec8287199dc402fe6a587902e300749.png?size=64')
+                .setColor('#02C2FF')
+                .setAuthor('UniversNetwork', 'https://cdn.discordapp.com/app-icons/792994169659981846/63db7674caa58a0694d989693bc4b60a.png?size=64', 'https://minecraft-mp.com/server-s272254')
+                .setTitle('**Prefix:** `.`')
+                .addField('\u200B', '\u200B', true)
+                .addField('Developer List Of Commands', '\u200B', true)
+                .addField('\u200B', '\u200B', true)
+                .addField('`.reload` `.load`', 'Untuk me-reload semua command')
+                .setFooter('Made By ARVIN3108 ID', 'https://cdn.discordapp.com/avatars/700166055326384179/3ec8287199dc402fe6a587902e300749.png?size=64')
             message.author.send(embed)
         }
         const embed = new MessageEmbed()
@@ -34,8 +34,10 @@ module.exports = {
             .addField('`.leave` `.disconnect` `.dc` `.stop`', 'Untuk menghentikan dan men-disconnect bot setelah memutar lagu')
             .addField('`.vote`', 'Untuk mengunjungi link vote server ini')
             .addField('`.info`', 'Untuk melihat informasi dari server atau bot\n\u200B\n\u200B')
-            .addField('\u200B', '\u200B',true)
-            .addField('Admin List Of Commands', '\u200B',true)
+            .addField('\u200B', '\u200B', true)
+
+        const embeds = new MessageEmbed()
+            .addField('Admin List Of Commands', '\u200B', true)
             .addField('\u200B', '\u200B', true)
             .addField('`.kick`', 'Untuk meng-kick member')
             .addField('`.ban`', 'Untuk meng-ban member')
@@ -48,6 +50,6 @@ module.exports = {
             .addField('`.verification` `.verify`', 'Untuk meng-set channel verifikasi (mirip dengan carl bot)')
             .addField('Check The Wiki For English Version', 'https://github.com/ARVIN3108/UniversNetwork/blob/main/README.md')
             .setFooter('Made By ARVIN3108 ID', 'https://cdn.discordapp.com/avatars/700166055326384179/3ec8287199dc402fe6a587902e300749.png?size=64')
-        message.delete().then(message.channel.send(embed))
+        message.delete().then(message.channel.send(embed)).then(message.channel.send(embeds))
     }
 }
