05f18ff5f3e143ab965e793e9469c8c54963849c
ARVIN3108 <70049331+ARVIN3108@users.noreply.github.com>
2021-01-22 15:36:51 +0700

    Update main.js

--- main.js
+++ main.js
@@ -88,47 +88,14 @@ const Discord = require('discord.js'),
     Prefix = process.env.Prefix,
     Version = process.env.Version
 
-// client.on('guildMemberAdd', async (member) => {
-//     const { guild } = member
-//     console.log(member)
-//     let channel = '761875413223735296'
-//     if (member.guild.id != '761872006513033238') return
-//     if (!channel) return
-//     const canvas = Canvas.createCanvas(555, 260)
-//     const ctx = canvas.getContext('2d');
+client.on('guildMemberAdd', async (member) => {
+    const { guild } = member
+    if (member.guild.id != '761872006513033238') return
+    const role = member.guild.roles.cache.find(role => role.id === '761876904465661962')
+    member.guild.members.cache.get(member.id).roles.add(role)
 
-//     const background = await Canvas.loadImage(
-//         path.join(__dirname, './background.png')
-//     )
+})
 
-//     let x = 0
-//     let y = 0
-//     ctx.drawImage(background, x, y)
-
-//     const pfp = await Canvas.loadImage(
-//         member.user.displayAvatarURL({
-//             format: 'png',
-//         })
-//     )
-//     x = canvas.width / 2 - pfp.width / 2
-//     y = 25
-//     ctx.drawImage(pfp, x, y)
-
-//     ctx.fillStyle = '#ffffff' // White text
-//     ctx.font = '35px sans-serif'
-//     let text = `Welcome ${member.user.tag}!`
-//     x = canvas.width / 2 - ctx.measureText(text).width / 2
-//     ctx.fillText(text, x, 60 + pfp.height)
-
-// // Display member count
-//     ctx.font = '30px sans-serif'
-//     text = `Member #${guild.memberCount}`
-//     x = canvas.width / 2 - ctx.measureText(text).width / 2
-//     ctx.fillText(text, x, 100 + pfp.height)
-
-//     const attachment = new MessageAttachment(canvas.toBuffer())
-//     client.channels.cache.get(channel).send('', attachment)
-// })
 function loadCMD() {
     const fs = require('fs'),
         ms = require('ms')
