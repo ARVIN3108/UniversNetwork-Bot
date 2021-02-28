82a1160c645d5cf78e3e2159a8b0ff401b4840f3
ARVIN3108 <70049331+ARVIN3108@users.noreply.github.com>
2021-02-28 13:28:30 +0700

    Add files via upload

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'ping',
     description: 'To Calculate Bot & API ping',
-    execute(message, client, MessageEmbed, Icon) {
+    execute(message, client, MessageEmbed, Prefix, Icon) {
         message.delete()
         message.channel.send(':hourglass: **Menghitung Ping Dalam 5 Detik...**').then(resultMessage => {
             const ping = resultMessage.createdTimestamp - message.createdTimestamp
@@ -10,7 +10,7 @@ module.exports = {
                 const embed = new MessageEmbed()
                     .setAuthor('UniversNetwork', 'https://cdn.discordapp.com/app-icons/792994169659981846/eccf642340521c532b0ade8f00591114.png?size=64', 'https://minecraft-mp.com/server-s272254')
                     .setColor('#15FF02')
-                    .setTitle('**Prefix:** `.`')
+                    .setTitle('**Prefix:** `' + Prefix + '`')
                     .setDescription(':clipboard: **Diminta Oleh** <@' + message.author + '>\n\u200B\n\u200B')
                     .addField('🤖 Bot Latency', ping, true)
                     .addField('🌐 API Latency', client.ws.ping, true)
